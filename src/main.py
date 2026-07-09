d0969321211bcf56552f037d26d27242f4651322
Juan Pablo Landa <juanpi9landa@gmail.com>
2026-07-08 23:29:42 -0300

    Refactor main.py to use probar_navegador function and add new browser, config, drive, flow, and prompt_generator modules

--- src/main.py
+++ src/main.py
@@ -1,13 +1,3 @@
-from playwright.sync_api import sync_playwright
+from browser import probar_navegador
 
-with sync_playwright() as p:
-
-    browser = p.chromium.launch(headless=True)
-
-    page = browser.new_page()
-
-    page.goto("https://example.com")
-
-    print(page.title())
-
-    browser.close()
+probar_navegador()
