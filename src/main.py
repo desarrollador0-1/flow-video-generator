4aebd99d8010f4062611f04f7a2ca18b790f229f
Juan Pablo Landa <juanpi9landa@gmail.com>
2026-07-08 23:24:41 -0300

    Add Playwright setup and update main.py to navigate to a webpage

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Hola desde GitHub Actions 🚀")
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto("https://example.com")
+
+    print(page.title())
+
+    browser.close()
